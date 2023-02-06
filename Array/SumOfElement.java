3a37f4221ddc6fe783e22f39b4c81cf86e2d55e3
ashishrajtkd <65500234+ashishrajtkd@users.noreply.github.com>
2023-02-06 10:14:21 +0000

    ArraysSum

---
+++ Array/SumOfElement.java
@@ -0,0 +1,10 @@
+class SumOfElement{
+    public static void main(String[] args) {
+        int a[]={1,5,7,4,7,3,8,3,7};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+        System.out.println(sum);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Java-Questions
-Only java program
